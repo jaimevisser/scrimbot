aa3eb34647cca8d3cf10e07c0c5e69dec65719a8
Jaime Visser <jaimevisser@gmail.com>
2022-01-12 19:42:18 +0100

    refactored time tags

--- scrimbot/tag.py
+++ scrimbot/tag.py
@@ -2,11 +2,16 @@ import math
 from datetime import datetime
 
 
-class Format:
+class TimeFormat:
     TIME = "t"
+    TIME_LONG = "T"
+    DATE = "d"
+    DATE_LONG = "D"
+    FULL = "f"
+    FULL_LONG = "F"
     RELATIVE = "R"
 
 
-def time(t: datetime, format="t"):
+def time(t: datetime, time_format=TimeFormat.TIME):
     utc = math.floor(t.timestamp())
-    return f"<t:{utc}:{format}>"
+    return f"<t:{utc}:{time_format}>"
